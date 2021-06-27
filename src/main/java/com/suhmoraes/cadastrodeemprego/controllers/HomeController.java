40aca654bea283d66037903dd0b82bb7a47b1445
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-06-27 12:58:59 -0300

    Lesson Spring Security: User and Password

--- src/main/java/com/suhmoraes/cadastrodeemprego/controllers/HomeController.java
+++ src/main/java/com/suhmoraes/cadastrodeemprego/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.suhmoraes.cadastrodeemprego.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class HomeController {
 	public String homeApp(Model model) {
 		model.addAttribute("mensagem", "Essa foi uma mensagem injetada através do Model");
 		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
---
+++ src/main/java/com/suhmoraes/cadastrodeemprego/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.suhmoraes.cadastrodeemprego.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public static BCryptPasswordEncoder passwordEnconder() {
+		return new BCryptPasswordEncoder();		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+	}
+	
+	//Autenticação do Usuário	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("user")
+		.password(passwordEnconder().encode("password")).authorities("USER");
+	}
+}
